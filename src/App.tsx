f78f37023decc62f733dfa4afc6bbd1efc043959
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-07-30 22:04:24 -0400

    Can now drag mouse to test move all plane vertices

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
     return (
         <div className="App">
             <WebcamVideo outVideoRef={videoRef} />
-            <DrawCanvas outDrawCanvasRef={drawCanvasRef} />
+            {/*<DrawCanvas outDrawCanvasRef={drawCanvasRef} />*/}
             <RendererCanvas videoRef={videoRef} drawCanvasRef={drawCanvasRef} />
         </div>
     );
--- src/RendererCanvas.tsx
+++ src/RendererCanvas.tsx
@@ -1,6 +1,5 @@
 import React, { RefObject, useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { vertex, fragment } from "./assets/DistortionShader.glsl";
 
 interface RendererCanvasProps {
     videoRef: RefObject<HTMLVideoElement>;
@@ -9,10 +8,17 @@ interface RendererCanvasProps {
 
 export default function RendererCanvas({ videoRef, drawCanvasRef }: RendererCanvasProps) {
     const parentRef = useRef<HTMLDivElement>(null);
+    const isDragging = useRef<boolean>(false);
+    const dragPosition = useRef<THREE.Vector2>(new THREE.Vector2(0, 0));
+    const planeVertexPositions = useRef<THREE.BufferAttribute>(new THREE.BufferAttribute(new Float32Array(0), 3));
 
     useEffect(() => {
         const renderer = createThreeRenderer();
-
+        
+        if (parentRef.current !== null) {
+            parentRef.current.appendChild(renderer.domElement);
+        }
+        
         return () => {
             if (parentRef.current !== null) {
                 parentRef.current.removeChild(renderer.domElement);
@@ -21,61 +27,90 @@ export default function RendererCanvas({ videoRef, drawCanvasRef }: RendererCanv
     }, []);
     
     function createThreeRenderer() : THREE.WebGLRenderer {
-        const camera = new THREE.OrthographicCamera(0, 1, 1, 0);
-        camera.position.z = 2;
+        const camera = new THREE.OrthographicCamera();
+        camera.position.z = 1;
 
         const scene = new THREE.Scene();
 
-        if (videoRef.current === null || drawCanvasRef.current === null) return new THREE.WebGLRenderer();
+        // TODO: if videoRef is null then just show a placeholder texture
+        // if (videoRef.current === null || drawCanvasRef.current === null) return new THREE.WebGLRenderer();
 
         // const videoTexture = new THREE.VideoTexture(videoRef.current);
         const videoTexture = new THREE.TextureLoader().load(require("./assets/Checkerboard.png"));
-        const drawCanvasTexture = new THREE.CanvasTexture(drawCanvasRef.current)
+        // const drawCanvasTexture = new THREE.CanvasTexture(drawCanvasRef.current)
 
-        const plane = new THREE.PlaneGeometry(1, 1);
-        const distortionMaterial = new THREE.ShaderMaterial({
-            uniforms: {
-                map: { value: videoTexture },
-                distortionMap: { value: drawCanvasTexture },
-                controlPoints: { value: [
-                        new THREE.Vector2(0, 0),
-                        new THREE.Vector2(0, 0.5),
-                        new THREE.Vector2(0, 1),
-                        new THREE.Vector2(0.5, 0),
-                        new THREE.Vector2(0.5, 0.5),
-                        new THREE.Vector2(0.5, 1),
-                        new THREE.Vector2(1, 0),
-                        new THREE.Vector2(1, 0.5),
-                        new THREE.Vector2(1, 1)
-                    ]},
-                dimensionX: { value: 3 },
-                dimensionY: { value: 3 }
-            },
-            vertexShader: vertex,
-            fragmentShader: fragment
-        });
-        const mesh = new THREE.Mesh(plane, distortionMaterial);
-        mesh.position.x = 0.5;
-        mesh.position.y = 0.5;
+        const plane = new THREE.PlaneGeometry(2, 2, 10, 10);
+        planeVertexPositions.current = plane.getAttribute("position") as THREE.BufferAttribute;
+        
+        for ( let i = 0; i < planeVertexPositions.current.count; i ++ ) {
+            let x = planeVertexPositions.current.getX(i) + 0.1*(Math.random()-0.5);
+            let y = planeVertexPositions.current.getY(i) + 0.1*(Math.random()-0.5);
+            // let z = positionAttribute.getZ(i) + 0.1*(Math.random()-0.5);
+            // positionAttribute.setXYZ(i, x, y, z);
+            planeVertexPositions.current.setXY(i, x, y);
+        }
+        
+        const mesh = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({ map: videoTexture }));
+        // const mesh = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({ wireframe: true }));
         scene.add(mesh);
 
         const renderer = new THREE.WebGLRenderer({antialias: true});
         
         if (parentRef.current !== null) {
             renderer.setSize(parentRef.current.clientWidth, parentRef.current.clientHeight);
-            parentRef.current.appendChild(renderer.domElement);
         }
 
         const animate = () => {
             requestAnimationFrame(animate);
             // videoTexture.needsUpdate = true;
-            drawCanvasTexture.needsUpdate = true;
+            // drawCanvasTexture.needsUpdate = true;
             renderer.render(scene, camera);
         }
 
         animate();
 
+        renderer.domElement.addEventListener("mousedown", startDrag);
+        renderer.domElement.addEventListener("mousemove", onDrag);
+        renderer.domElement.addEventListener("mouseup", stopDrag);
+
         return renderer;
+    }
+
+    function startDrag(event: MouseEvent) {
+        isDragging.current = true;
+        recalculateDrawPosition(event);
+    }
+
+    function onDrag(event: MouseEvent) {
+        if (!isDragging.current) return;
+        
+        const prevDrawPosition = dragPosition.current.clone();
+        recalculateDrawPosition(event);
+        
+        let dragDelta: THREE.Vector2 = dragPosition.current.clone().sub(prevDrawPosition);
+        
+        // TODO: modify vertices
+        for ( let i = 0; i < planeVertexPositions.current.count; i ++ ) {
+            
+            let x = planeVertexPositions.current.getX(i) + 0.001 * dragDelta.x;
+            let y = planeVertexPositions.current.getY(i) - 0.001 * dragDelta.y;
+            // let z = positionAttribute.getZ(i) + 0.1*(Math.random()-0.5);
+            // positionAttribute.setXYZ(i, x, y, z);
+            planeVertexPositions.current.setXY(i, x, y);
+        }
+        
+        planeVertexPositions.current.needsUpdate = true;
+    }
+
+    function stopDrag() {
+        isDragging.current = false;
+    }
+
+    function recalculateDrawPosition(event: MouseEvent) {
+        if (parentRef.current === null) return;
+
+        dragPosition.current.x = event.clientX - parentRef.current.offsetLeft;
+        dragPosition.current.y = event.clientY - parentRef.current.offsetTop;
     }
 
     return (
