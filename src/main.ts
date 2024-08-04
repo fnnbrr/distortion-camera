ff94002fde62a0efa071adc5f0950d8040eba72a
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-08-04 19:16:53 -0400

    First pass on camera swapping

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import {requestWebcam} from "./webcam-video.ts";
+import {nextVideoInput, requestVideoInput} from "./webcam-video.ts";
 import {DistortionMesh} from "./distortionMesh.ts";
 import viteLogo from "./assets/vite.svg"
 
@@ -10,17 +10,20 @@ function main(): void {
     <video id="video" playsInline poster="${viteLogo}"></video>
     <button id="reset-button">reset</button>
     <button id="photo-button">take photo</button>
+    <button id="swap-camera-button">swap camera</button>
     `
     
     const parent = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
     const video = document.querySelector<HTMLVideoElement>('#video') as HTMLVideoElement;
     const resetButton = document.querySelector<HTMLButtonElement>('#reset-button') as HTMLButtonElement;
     const photoButton = document.querySelector<HTMLButtonElement>('#photo-button') as HTMLButtonElement;
+    const swapCameraButton = document.querySelector<HTMLButtonElement>('#swap-camera-button') as HTMLButtonElement;
     
-    requestWebcam(video);
+    requestVideoInput(video);
 
     const distortionMesh = new DistortionMesh(video, parent);
     
     resetButton.addEventListener("click", () => distortionMesh.resetVertices());
     photoButton.addEventListener("click", () => distortionMesh.takePhoto());
+    swapCameraButton.addEventListener("click", () => nextVideoInput());
 }
--- src/webcam-video.ts
+++ src/webcam-video.ts
@@ -1,18 +1,24 @@
-﻿export async function requestWebcam(videoElement: HTMLVideoElement) {
+﻿const constraints: MediaStreamConstraints = {
+    video: {
+        width: 1280,
+        height: 720,
+        facingMode: "user"
+    }
+};
+
+const videoDevices: MediaDeviceInfo[] = [];
+let videoDeviceIndex = 0;
+
+let stream: MediaStream;
+let video: HTMLVideoElement;
+
+export async function requestVideoInput(videoElement: HTMLVideoElement) {
     if (navigator.mediaDevices === undefined) {
         console.error(`Cannot access navigator.mediaDevices. Make sure you're using HTTPS.`);
         return;
     }
     
-    const constraints: MediaStreamConstraints = {
-        video: { 
-            width: 1280,
-            height: 720,
-            facingMode: "user"
-        }
-    };
-    
-    const videoDevices: MediaDeviceInfo[] = [];
+    video = videoElement;
     
     try {
         await navigator.mediaDevices.getUserMedia(constraints);
@@ -30,10 +36,26 @@
             return;
         }
         
-        videoElement.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
-        videoElement.play()
+        stream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = stream;
+        video.play()
             .catch(err => console.error(`Error playing video: ${err}`));
     } catch (e) {
         console.error(`Error accessing webcam: ${e}`)
     }
+}
+
+export async function nextVideoInput() {
+    if (videoDevices.length < 2) return;
+    
+    stream.getTracks().forEach(track => track.stop());
+
+    videoDeviceIndex += 1;
+    videoDeviceIndex %= videoDevices.length;
+    (constraints.video as MediaTrackConstraints).deviceId = { exact: videoDevices[videoDeviceIndex].deviceId };
+    
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = stream;
+    video.play()
+        .catch(err => console.error(`Error playing video: ${err}`));
 }
