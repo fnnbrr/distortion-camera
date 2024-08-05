6cf9f590725ec55046dbf331d2aff3350ab8619c
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-08-05 15:09:05 -0400

    Refactored video input handling

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import './style.css'
-import {nextVideoInput, requestVideoInput} from "./webcam-video.ts";
-import {DistortionMesh} from "./distortionMesh.ts";
-import viteLogo from "./assets/vite.svg"
+import {VideoInput} from "./video-input.ts";
+import {DistortionMesh} from "./distortion-mesh.ts";
+import cameraSwitchIcon from "./assets/camera-switch-svgrepo-com.svg";
+import deleteIcon from "./assets/delete-svgrepo-com.svg";
 
 main();
 
-function main(): void {
+async function main() {
     document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <video id="video" playsInline poster="${viteLogo}"></video>
-    <button id="reset-button">reset</button>
+    <video id="video" playsInline poster="${cameraSwitchIcon}"></video>
+    <button id="reset-button"><img src="${deleteIcon}"></button>
     <button id="photo-button">take photo</button>
-    <button id="swap-camera-button">swap camera</button>
+    <button id="swap-camera-button"><img src="${cameraSwitchIcon}"></button>
     `
     
     const parent = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
@@ -19,11 +20,23 @@ function main(): void {
     const photoButton = document.querySelector<HTMLButtonElement>('#photo-button') as HTMLButtonElement;
     const swapCameraButton = document.querySelector<HTMLButtonElement>('#swap-camera-button') as HTMLButtonElement;
     
-    requestVideoInput(video);
-
+    const videoInput = new VideoInput(video);
     const distortionMesh = new DistortionMesh(video, parent);
     
     resetButton.addEventListener("click", () => distortionMesh.resetVertices());
     photoButton.addEventListener("click", () => distortionMesh.takePhoto());
-    swapCameraButton.addEventListener("click", () => nextVideoInput());
+    
+    try {
+        await videoInput.startNextVideoDevice();
+        
+        if (videoInput.hasMultipleVideoDevices()) {
+            swapCameraButton.addEventListener("click", () => videoInput.startNextVideoDevice());
+        }
+        else {
+            swapCameraButton.remove();
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
---
+++ src/video-input.ts
@@ -0,0 +1,75 @@
+﻿export class VideoInput {
+    #constraints: MediaStreamConstraints = {
+        video: {
+            width: 1280,
+            height: 720,
+            facingMode: "user"
+        }
+    };
+
+    #hasQueriedVideoDevices: boolean = false;
+    #videoDevices: MediaDeviceInfo[] = [];
+    #videoDeviceIndex: number = 0;
+
+    #video: HTMLVideoElement;
+    #stream: MediaStream = new MediaStream();
+    
+    constructor(videoElement: HTMLVideoElement) {
+        this.#video = videoElement;
+
+        if (navigator.mediaDevices === undefined) {
+            console.error(`Cannot access navigator.mediaDevices. Make sure you're using HTTPS.`);
+            return;
+        }
+    }
+    
+    async startNextVideoDevice() {
+        try {
+            // Clean up previous stream
+            this.#stream.getTracks().forEach(track => track.stop());
+            
+            if (this.#hasQueriedVideoDevices && this.#videoDevices.length > 1) {
+                this.#updateConstraintsForNextDevice();
+            }
+            
+            this.#stream = await navigator.mediaDevices.getUserMedia(this.#constraints);
+            this.#video.srcObject = this.#stream;
+            this.#video.play()
+                .catch(err => console.error(`Error playing video: ${err}`));
+
+            if (!this.#hasQueriedVideoDevices) {
+                await this.#queryVideoDevices();
+            }
+        } catch (e) {
+            console.error(`Error accessing video device: ${e}`);
+        }
+    }
+    
+    hasMultipleVideoDevices(): boolean {
+        if (!this.#hasQueriedVideoDevices) {
+            console.log(`Hasn't queried video devices yet`);
+            return false;
+        }
+        
+        return this.#videoDevices.length > 1;
+    }
+    
+    async #queryVideoDevices() {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        
+        devices.forEach((device) => {
+            if (device.kind === "videoinput") {
+                this.#videoDevices.push(device);
+            }
+        });
+        
+        this.#hasQueriedVideoDevices = true;
+    }
+    
+    #updateConstraintsForNextDevice() {
+        this.#videoDeviceIndex += 1;
+        this.#videoDeviceIndex %= this.#videoDevices.length;
+        (this.#constraints.video as MediaTrackConstraints).deviceId = {
+            exact: this.#videoDevices[this.#videoDeviceIndex].deviceId };
+    }
+}
